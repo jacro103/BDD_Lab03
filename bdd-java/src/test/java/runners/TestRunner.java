78e4c0a28cb763c3d07d2da3bd5efea4a5f635bc
jacro103 <89158012+jacro103@users.noreply.github.com>
2025-03-01 17:44:45 +0000

    avance lab

---
+++ bdd-java/src/test/java/runners/TestRunner.java
@@ -0,0 +1,16 @@
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/features", glue="steps",
+monochrome = true,
+publish = true,
+plugin = {"pretty", "junit:target/JUnitReports/report.xml",
+        "json:target/JSonReports/report.json",
+        "html:target/HtmlReports/report.html"
+        }
+)
+public class TestRunner {
+}
